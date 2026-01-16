11b3fa478884c155fab18d08affea5dc038e36e6
Camputer07 <chr215@exeter.ac.uk>
2026-01-16 13:13:52 +0000

    Completed game_3.py

--- game_functions.py
+++ game_functions.py
@@ -16,4 +16,22 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    # Keeps track of if the letter is in the word
+    letter_present = False
+    # Loop through each letter in the hangman word
+    for i in range(len(word)):
+        if letter == word[i]:
+            # Update the board at this location with 'letter'
+            board[i] = letter
+            # Update the boolean tracker variable
+            letter_present = True
+    
+    # Check if the letter is present
+    if letter_present is True:
+        # Output success message
+        print(f"Well done! '{letter}' is in the word")
+        return True
+    else:
+        # Output failed message
+        print(f"Sorry, '{letter}' is not in the word")
+        return False
