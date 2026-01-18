d308b4823dd34bb5e045dfc1f94893f3bc5f4403
Camputer07 <chr215@exeter.ac.uk>
2026-01-18 19:59:31 +0000

    Completed game_1.py pick_value function.

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.randint(0, 20)
+    x = random.randint(poss_values)
     return x
 
 # function to be used in game_2: Higher or Lower
